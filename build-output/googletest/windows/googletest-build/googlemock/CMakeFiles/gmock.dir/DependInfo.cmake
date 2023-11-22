287229e98fc3ff8098eeb25ac6478afd0dd92276
John-Brian Vyncent <jbvyncent@vianovation.com>
2023-11-22 07:27:53 -0500

    Initial load of sql-server-language-extensions

---
+++ build-output/googletest/windows/googletest-build/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/GitHub-Vianovation/sql-server-language-extensions/build-output/googletest/windows/googletest-src/googlemock/src/gmock-all.cc" "F:/GitHub-Vianovation/sql-server-language-extensions/build-output/googletest/windows/googletest-build/googlemock/CMakeFiles/gmock.dir/src/gmock-all.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest-src/googlemock/include"
+  "googletest-src/googlemock"
+  "googletest-src/googletest/include"
+  "googletest-src/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/GitHub-Vianovation/sql-server-language-extensions/build-output/googletest/windows/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-output/googletest/windows/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/GitHub-Vianovation/sql-server-language-extensions/build-output/googletest/windows/googletest-src/googletest
+# Build directory: F:/GitHub-Vianovation/sql-server-language-extensions/build-output/googletest/windows/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
